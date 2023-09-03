5ea19f3f6c824444d2ffcfe7d03bad70b53c07d1
David G. Moore, Jr <david@dgmjr.io>
2023-09-03 12:48:02 -0400

    ADADDED PhoneNumberTests.cs ADDED PrimitivesTests.cs ADDED urlTests.cs ADDED urnTests.cs ADDED uriTests.cs

---
+++ Tests/HexStringTests.cs
@@ -0,0 +1,35 @@
+/*
+ * HexStringTests.cs
+ *
+ *   Created: 2023-09-03-12:33:55
+ *   Modified: 2023-09-03-12:33:55
+ *
+ *   Author: David G. Moore, Jr. <david@dgmjr.io>
+ *
+ *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */
+
+namespace Dgmjr.Primitives.Tests;
+
+public static class HexStringTests
+{
+    [Theory]
+    [InlineData("0123456789abcdef")]
+    [InlineData("ffffaabb")]
+    [InlineData("FFffAAbb")]
+    public static void Valid_Hex_String_Is_Valid(string validHexString)
+    {
+        var hexString = new HexString(validHexString);
+        hexString.Value.Should().Be(validHexString);
+    }
+    [Theory]
+    [InlineData("fcku!!!")]
+    [InlineData("go the fuck away!")]
+    [InlineData("NOW")]
+    public static void Invalid_Hex_String_Is_Invalid(string invalidHexString)
+    {
+        var hexStringCtor = () => new HexString(invalidHexString);
+        hexStringCtor.Should().Throw<ArgumentException>();
+    }
+}
---
+++ Tests/PhoneNumberTests.cs
@@ -0,0 +1,34 @@
+/*
+ * EmailAddressTests copy.cs
+ *
+ *   Created: 2023-09-02-03:25:55
+ *   Modified: 2023-09-02-03:25:56
+ *
+ *   Author: David G. Moore, Jr. <david@dgmjr.io>
+ *
+ *   Copyright © 2022 - 2023 David G. Moore, Jr., All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */
+
+namespace Dgmjr.Primitives.Tests;
+
+using System.Domain;
+
+public class PhoneNumberTests : PrimitivesTests<PhoneNumber, PhoneNumberTests>
+{
+    public PhoneNumberTests(ITestOutputHelper output) : base(output) { }
+
+    protected override string[][] InvalidValuesStrings => new[]
+    {
+        new[] { "not-a-phone-number" },
+        new[] { "+1+9172125869" }
+    };
+
+    protected override string[][] ValidValuesStrings => new[]
+    {
+        new[] { "+1 202-503-4657" },
+        new[] { "+12025034657" },
+        new[] { "+1 202.503.4657" },
+        new[] { "+1 202 503 4657" }
+    };
+}
