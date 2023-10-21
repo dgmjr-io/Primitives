99757dc0f725cd7c340011973539aa1bb84143f9
David G. Moore, Jr <david@dgmjr.io>
2023-10-21 10:49:22 -0400

    Refactor error handling in EmailAddress and PhoneNumber parsing methods
    This commit refactors the error handling in the parsing methods of the EmailAddress and PhoneNumber classes. Instead of using a catch-all exception, specific exceptions are caught and handled accordingly. This improves the robustness and maintainability of the code.

--- src/EmailAddress.cs
+++ src/EmailAddress.cs
@@ -247,10 +247,14 @@ public partial record struct EmailAddress : IStringWithRegexValueObject<EmailAdd
         {
             return From(s);
         }
-        catch
-        {
-            return Empty;
-        }
+        catch(ValueObjectValidationException) { return Empty; }
+        catch(ArgumentNullException) { return Empty; }
+        catch(FormatException) { return Empty; }
+        catch(OverflowException) { return Empty; }
+        catch(ArgumentException) { return Empty; }
+        catch(InvalidCastException) { return Empty; }
+        catch(InvalidOperationException) { return Empty; }
+        catch(Exception) { return Empty; }
     }
 
     public static implicit operator string?(EmailAddress? addr) =>
